afbfda6c1c361102ce848b41c86a29080dd57e8a
Kanika Khurana <150192203+kanikakhurana5@users.noreply.github.com>
2025-07-23 20:48:05 +0530

    Add files via upload

---
+++ EventtrackerApplication.java
@@ -0,0 +1,11 @@
+package com.example.eventtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventtrackerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EventtrackerApplication.class, args);
+    }
+}
